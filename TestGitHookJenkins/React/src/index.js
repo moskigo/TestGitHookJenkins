ec3bbcca258a7e39fc9f707dceb247c8daa3c9dc
Ihor Moskvita <moskigo2@gmail.com>
2018-08-14 17:16:07 +0300

    build 19

--- TestGitHookJenkins/React/src/index.js
+++ TestGitHookJenkins/React/src/index.js
@@ -6,4 +6,4 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-//react auto build 18
+//react auto build 19
