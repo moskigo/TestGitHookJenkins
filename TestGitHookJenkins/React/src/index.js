9c0797335065916e5b41a5ee8d01aff15c4abcf9
Ihor Moskvita <moskigo2@gmail.com>
2018-08-14 15:09:24 +0300

    build 11

--- TestGitHookJenkins/React/src/index.js
+++ TestGitHookJenkins/React/src/index.js
@@ -6,4 +6,4 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-//react auto build 10
+//react auto build 11
